a16dbd9f65ca23ebff6372e500e661a9126f7661
amh1k <abdulmoizx97@gmail.com>
2026-05-12 20:28:46 +0500

    feat: extract player store and add win recording support

--- main.go
+++ main.go
@@ -4,14 +4,15 @@ import (
 	"log"
 	"net/http"
 )
-type InMemoryPlayerStore struct{}
 
-func (i *InMemoryPlayerStore)GetPlayerScore(name string)int{
-	return 123
-}
+// func (i *InMemoryPlayerStore)GetPlayerScore(name string)int{
+// 	return 123
+// }
+// func (i *InMemoryPlayerStore) RecordWin(name string) {}
 func main() {
-	server := &PlayerServer{&InMemoryPlayerStore{}}
+	server := &PlayerServer{NewInMemoryPlayerStore()}
+	log.Fatal(http.ListenAndServe(":5000", server))
 	//http.HandlerFunc is basically a type which is converting PlayerServer to a function recognized by http.ListenAndServer
 	// We are basically type casting
-	log.Fatal(http.ListenAndServe(":5000", server))
+	
 }
--- server.go
+++ server.go
@@ -7,23 +7,36 @@ import (
 )
 type PlayerStore interface {
 	GetPlayerScore(name string) int
+	RecordWin(name string)
+
 }
 type PlayerServer struct {
 	store PlayerStore
 }
 func (p *PlayerServer)ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
-	fmt.Fprint(w, p.store.GetPlayerScore(player))
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w, player)
+	case http.MethodGet:
+		p.showScore(w, player)
+	}
+	
 
 }
-func GetPlayerScore(name string) string {
-	if name == "Pepper" {
-		return "20"
+func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
+	
+	score := p.store.GetPlayerScore(player)
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
 	}
-	if name == "Floyd" {
-		return "10"
-	}
-	return ""
+	fmt.Fprint(w, score)
+
+}
+func (p *PlayerServer) processWin(w http.ResponseWriter, player string) {
+	
+	p.store.RecordWin(player)
+	w.WriteHeader(http.StatusAccepted)
 }
 
 // type Handler interface {
