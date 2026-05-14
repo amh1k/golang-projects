f777c1ea79d6499f7f45a30e88440bce2de05336
amh1k <abdulmoizx97@gmail.com>
2026-05-14 18:40:57 +0500

    feat: add league endpoint and JSON response support

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 // }
 // func (i *InMemoryPlayerStore) RecordWin(name string) {}
 func main() {
-	server := &PlayerServer{NewInMemoryPlayerStore()}
+	server := NewPlayerServer(NewInMemoryPlayerStore())
 	log.Fatal(http.ListenAndServe(":5000", server))
 	//http.HandlerFunc is basically a type which is converting PlayerServer to a function recognized by http.ListenAndServer
 	// We are basically type casting
--- server.go
+++ server.go
@@ -1,19 +1,54 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strings"
 )
+const jsonContentType = "application/json"
+
+type Player struct {
+	Name string
+	Wins int
+}
 type PlayerStore interface {
 	GetPlayerScore(name string) int
 	RecordWin(name string)
+	GetLeague() []Player
 
 }
 type PlayerServer struct {
 	store PlayerStore
+	http.Handler
 }
-func (p *PlayerServer)ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func NewPlayerServer(store PlayerStore) *PlayerServer {
+	p := new(PlayerServer)
+	p.store = store
+	router := http.NewServeMux()
+	router.Handle("/league", http.HandlerFunc(p.leagueHandler))
+	router.Handle("/players/", http.HandlerFunc(p.playersHandler))
+	p.Handler = router
+	return p
+
+}
+// func (p *PlayerServer)ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	p.router.ServeHTTP(w, r)
+// 	// router := http.NewServeMux()
+// 	// router.Handle("/league", http.HandlerFunc(p.leagueHandler))
+// 	// router.Handle("/players/", http.HandlerFunc(p.playersHandler))
+// 	// router.ServeHTTP(w, r)
+	
+	
+
+// }
+func (p *PlayerServer) leagueHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-type", "application/json")
+	json.NewEncoder(w).Encode(p.store.GetLeague())
+	
+}
+
+func (p *PlayerServer) playersHandler(w http.ResponseWriter, r *http.Request) {
 	player := strings.TrimPrefix(r.URL.Path, "/players/")
 	switch r.Method {
 	case http.MethodPost:
@@ -21,7 +56,6 @@ func (p *PlayerServer)ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	case http.MethodGet:
 		p.showScore(w, player)
 	}
-	
 
 }
 func (p *PlayerServer) showScore(w http.ResponseWriter, player string) {
