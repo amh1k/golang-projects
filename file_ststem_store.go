5b8fa3687398bb2400813ba945470636ef3ee4ec
amh1k <abdulmoizx97@gmail.com>
2026-05-14 19:40:34 +0500

    feat: add file system store implementation and tests

---
+++ file_ststem_store.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+)
+
+
+
+type FileSystemPlayerStore struct{
+	database io.ReadWriteSeeker
+}
+func (f *FileSystemPlayerStore)GetLeague()[]Player {
+	f.database.Seek(0, io.SeekStart)
+	league, _ := NewLeague(f.database)
+	return league
+
+
+}
+
+func NewLeague(rdr io.Reader)([]Player, error) {
+	var league[] Player
+	err := json.NewDecoder(rdr).Decode(&league)
+	if err != nil {
+		err = fmt.Errorf("problem parsing league, %v", err)
+	}
+
+	return league, err
+
+}
+
+func (f *FileSystemPlayerStore)GetPlayerScore(name string) int {
+	var wins int
+	for _, player := range f.GetLeague(){
+		if player.Name == name {
+			wins = player.Wins
+			break
+		}
+	}
+	return wins
+}
